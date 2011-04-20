f794d3ee75c4b7a4e7a12b95d3c099878b09cb17
Zach Azar <zachary.r.azar@vanderbilt.edu>
2011-04-20 12:45:04 -0500

    Introduce skeleton classes for publisher, handler, and message

---
+++ src/reactor/Message.java
@@ -0,0 +1,28 @@
+package reactor;
+
+public class Message {
+
+	private int type_;
+	public String msg_;
+	
+	public int getType()
+	{
+		return type_;
+	}
+	
+	public void setType(int type_)
+	{
+		this.type_ = type_;
+	}
+	
+	public String getMsg()
+	{
+		return msg_;
+	}
+	
+	public void setMsg(String msg_)
+	{
+		this.msg_ = msg_;
+	}
+	
+}
---
+++ src/reactor/Publisher.java
@@ -0,0 +1,28 @@
+package reactor;
+
+import java.util.List;
+
+public class Publisher {
+
+	protected List<Handler> subs_; //list of subscribers for this publisher
+	
+	public Publisher()
+	{
+	
+	}
+	
+	public void addSubscriber(Handler h)
+	{
+		
+	}
+	
+	public void remSubscriber (Handler h)
+	{
+		
+	}
+	
+	public void accept(Message msg)
+	{
+		
+	}
+}
